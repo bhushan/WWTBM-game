27675f71d6cf5385e5d55eb4b891b97e93b54bb4
Bhushan <bhushangaykawad@gmail.com>
2022-08-07 20:58:15 +0530

    refactor: refactored a code little bit

--- wwtbm.js
+++ wwtbm.js
@@ -32,13 +32,177 @@ const questions = [
     },
     correctAnswer: "a",
   },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "question asked Level 3",
+    level: 3,
+    options: {
+      a: "option a",
+      b: "option b",
+      c: "option c",
+      d: "option d",
+    },
+    correctAnswer: "a",
+  },
 ];
-
-const questionsAskedInEachLevel = 5;
 
 let currentLevel = 1;
 let optionSelected = false;
 let currentQuestion = {};
+const questionsAskedInEachLevel = 5;
 
 const questionElement = document.querySelector(".question");
 const optionElements = document.querySelectorAll(".option");
@@ -52,8 +216,6 @@ const resetState = () => {
   );
 };
 
-// start()
-// getRandomQuestion from question bank with level
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -61,61 +223,51 @@ function getRandomArbitrary(min, max) {
 const getRandomQuestion = (level = 1) => {
   // TODO: use levels to fetch correct level question
   const questionsCount = questions.length;
+
   if (questionsCount === 0) {
     alert(
       "All questions have been asked already, fill up question bank with more questions"
     );
+
     return {};
   }
 
   const randomIndex = getRandomArbitrary(0, questionsCount);
-
   const question = questions[randomIndex];
-
   questions.splice(randomIndex, 1);
 
   return question;
 };
 
-const attachEventListForSelection = () => {
-  optionElements.forEach((optionElement) =>
-    optionElement.addEventListener("click", function () {
-      if (optionSelected) return;
-      optionElement.classList.add("selected");
-      optionSelected = true;
-      if (Object.keys(currentQuestion).length === 0) {
-        return;
-      }
-      const { correctAnswer } = currentQuestion;
-      const correctOptionElement = document.querySelector(
-        `.option .${correctAnswer}`
-      );
-      correctOptionElement.parentNode.classList.remove("selected");
-      correctOptionElement.parentNode.classList.add("correct");
-    })
+function submitAnswer(event) {
+  if (optionSelected) return;
+  let optionElement = event.target;
+
+  if (optionElement !== this) {
+    optionElement = event.target.parentNode;
+  }
+
+  optionElement.classList.add("selected");
+  optionSelected = true;
+  if (Object.keys(currentQuestion).length === 0) {
+    return;
+  }
+  const { correctAnswer } = currentQuestion;
+  const correctOptionElement = document.querySelector(
+    `.option .${correctAnswer}`
   );
-};
+  correctOptionElement.parentNode.classList.remove("selected");
+  correctOptionElement.parentNode.classList.add("correct");
+}
 
-const attachEventListnerForNextQuestion = () => {
-  questionElement.addEventListener("click", function () {
-    if (!optionSelected) return;
-    resetState();
-    currentQuestion = getRandomQuestion(currentLevel);
-    if (Object.keys(currentQuestion).length === 0) {
-      return;
-    }
-    displayQuestion(currentQuestion);
-  });
-};
-
-const start = () => {
+const proceedToNextQuestion = () => {
+  if (!optionSelected) return;
+  resetState();
   currentQuestion = getRandomQuestion(currentLevel);
   if (Object.keys(currentQuestion).length === 0) {
     return;
   }
   displayQuestion(currentQuestion);
-  attachEventListForSelection();
-  attachEventListnerForNextQuestion();
 };
 
 const displayQuestion = ({ question, options }) => {
@@ -132,4 +284,18 @@ const displayQuestion = ({ question, options }) => {
   optionDElement.innerText = options.d;
 };
 
-start();
+optionElements.forEach((optionElement) =>
+  optionElement.addEventListener("click", submitAnswer)
+);
+questionElement.addEventListener("click", proceedToNextQuestion);
+
+const init = () => {
+  currentQuestion = getRandomQuestion(currentLevel);
+  if (Object.keys(currentQuestion).length === 0) {
+    return;
+  }
+
+  displayQuestion(currentQuestion);
+};
+
+init();
